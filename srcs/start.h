665fde8372d580c949e5e170d707e9e6fff6ef1a
alaafia <alaafia@student.1337.ma>
2020-09-28 20:46:53 +0100

    test

---
+++ srcs/start.h
@@ -0,0 +1,23 @@
+service mysql start
+sq=$?
+service nginx start
+ng=$?
+service php7.3-fpm start
+ph=$?
+
+while [ "$sq" = 0 ] && [ "$ng" = 0 ] &&  [ "$ph" = 0 ]
+do
+	sleep 10
+	service mysql status > /dev/null
+	sq=$?
+	service nginx status > /dev/null
+	ng=$?
+	service php7.3-fpm status > /dev/null
+	ph=$?
+done
+
+service mysql stop
+service php7.3-fpm stop
+service nginx stop
+
+echo -e "the server will have to stop"
